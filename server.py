9e593c56025bca77d560f1538c973ea8b31e8e21
Madhura Mande <mandemadhura@gmail.com>
2021-04-03 04:31:57 -0600

    Language_exchange
    - API to retrieve a single record of Language
    - API to get the records of all language
    
    Signed-off-by: Madhura Mande <mandemadhura@gmail.com>

--- server.py
+++ server.py
@@ -6,8 +6,12 @@ app = Flask(__name__)
 
 existing_language = {"1": "Marathi"}
 
-def success_response(status_code=None, lang_id=None):
+def success_response(status_code=None, lang_id=None, lang_name=None, lang_obj=None):
     message = {"error": '', "data": {"lang_id": lang_id}}
+    if lang_name is not None:
+        message['data']['lang_name'] = lang_name
+    if lang_obj is not None:
+        message['data'] = lang_obj 
     return message, status_code
 
 def error_response(error_string=None, status_code=None):
@@ -52,8 +56,21 @@ def update_language(lang_id):
     if lang_name in existing_language.values():
         return error_response('Language name must be unique', HTTPStatus.CONFLICT)
     return success_response(HTTPStatus.OK, lang_id)
+
+@app.route('/languages/<lang_id>', methods=['GET'])
+def get_a_language(lang_id): 
+    if lang_id not in existing_language.keys(): 
+        return error_response('Language does not exist', HTTPStatus.NOT_FOUND)
+    lang_name = existing_language[lang_id]
+    return success_response(HTTPStatus.OK, lang_id, lang_name)
     
-    
+@app.route('/languages/', methods=['GET'])
+def get_languages():
+    Language = []
+    for lang_id, lang_name in existing_language.items():
+        Language.append({'lang_id': lang_id, 'lang_name': lang_name})
+    return success_response(HTTPStatus.OK, lang_obj=Language)
+
 if __name__ == '__main__':
     app.run()
   
